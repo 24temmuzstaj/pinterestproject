70218248efea77e4ae687732b3a24aa15dd1ecf6
fatihpur <lojmangrisi@gmail.com>
2023-07-30 23:31:33 +0300

    login bg filter eklendi, yarin devam edielcek.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+/* OTURUM AÇ BUTONUNA TIKLANINCA AÇILAN BÖLÜMÜN JS KODULARI */
 const loginbutton = document.querySelector(".nav-oturum-ac");
 const login = document.querySelector(".navbar-login-side");
+const body = document.querySelector(".body");
 loginbutton.addEventListener("click", loginswitch);
 function loginswitch() {
   $(login).toggleClass("show");
+  $(body).toggleClass("body");
 }
