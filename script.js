ebf42e81d4ff7c8747bb98af29fd77d7674b748f
fatihpur <lojmangrisi@gmail.com>
2023-07-31 22:19:11 +0300

    register exit butonu ok

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const registerbutton = document.querySelector(".nav-kaydol");
 const login = document.querySelector(".navbar-login-side");
 const register = document.querySelector(".navbar-register-side");
 const exitbutton = document.querySelector(".exit-button");
+const register_exit_button = document.querySelector(".register-exit-button");
 
 loginbutton.addEventListener("click", loginswitch);
 registerbutton.addEventListener("click", registerswitch);
-exitbutton.addEventListener("click", exit);
+exitbutton.addEventListener("click", loginexit);
+register_exit_button.addEventListener("click", registerexit);
 
 function loginswitch() {
   $(login).toggleClass("show");
@@ -15,7 +17,11 @@ function loginswitch() {
 function registerswitch() {
   $(register).toggleClass("show");
 }
-function exit(event) {
+function loginexit(event) {
   $(login).toggleClass("show");
+  event.preventDefault();
+}
+function registerexit(event) {
+  $(register).toggleClass("show");
   event.preventDefault();
 }
