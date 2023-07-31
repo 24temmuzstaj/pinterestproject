e9d91319d902ab77c2668b2a049c007717db8cbd
fatihpur <lojmangrisi@gmail.com>
2023-07-31 17:56:32 +0300

    login-register ok, belki revize edilebilir

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 /* OTURUM AÇ BUTONUNA TIKLANINCA AÇILAN BÖLÜMÜN JS KODULARI */
 const loginbutton = document.querySelector(".nav-oturum-ac");
+const registerbutton = document.querySelector(".nav-kaydol");
 const login = document.querySelector(".navbar-login-side");
+const register = document.querySelector(".navbar-register-side");
 const exitbutton = document.querySelector(".exit-button");
-const body = document.querySelector(".body");
+
+loginbutton.addEventListener("click", loginswitch);
+registerbutton.addEventListener("click", registerswitch);
+exitbutton.addEventListener("click", exit);
+
 function loginswitch() {
   $(login).toggleClass("show");
-  $(body).toggleClass("body");
 }
-loginbutton.addEventListener("click", loginswitch);
-exitbutton.addEventListener("click", exit);
+function registerswitch() {
+  $(register).toggleClass("show");
+}
 function exit(event) {
   $(login).toggleClass("show");
   event.preventDefault();
