239406b8da2d4a1690393e4480a17d5118631a1f
fatihpur <lojmangrisi@gmail.com>
2023-07-31 13:56:03 +0300

    logindeki exit butonu aktif edildi.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 /* OTURUM AÇ BUTONUNA TIKLANINCA AÇILAN BÖLÜMÜN JS KODULARI */
 const loginbutton = document.querySelector(".nav-oturum-ac");
 const login = document.querySelector(".navbar-login-side");
+const exitbutton = document.querySelector(".exit-button");
 const body = document.querySelector(".body");
-loginbutton.addEventListener("click", loginswitch);
 function loginswitch() {
   $(login).toggleClass("show");
   $(body).toggleClass("body");
+}
+loginbutton.addEventListener("click", loginswitch);
+exitbutton.addEventListener("click", exit);
+function exit(event) {
+  $(login).toggleClass("show");
+  event.preventDefault();
 }
