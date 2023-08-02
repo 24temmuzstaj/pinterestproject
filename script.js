1fced2d6655539a91cb770d26559e6ff771fb6a6
mmehtapavci <mmmehtapavci@gmail.com>
2023-08-02 15:58:09 +0300

    slideranimationedit

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-$(document).ready(function () {
+
+$(document).ready(function() {
+    // Otomatik geçişi başlat
+    $('#carouselExampleCaptions').carousel();
+
     // Otomatik geçiş için fonksiyon
     function nextSlide() {
-        var activeSlide = $('.carousel-item.active');
-        var nextSlide = activeSlide.next('.carousel-item');
-        if (!nextSlide.length) {
-            // Eğer son slaytta ise, ilk slayta dön
-            nextSlide = $('.carousel-item').first();
-        }
-        activeSlide.removeClass('active');
-        nextSlide.addClass('active');
+        $('#carouselExampleCaptions').carousel('next');
     }
 
     // Otomatik geçiş aralığı (3 saniye)
     var interval = setInterval(nextSlide, 3000);
-
-    // İlk slaydı seçili yap
-    $('.carousel-item').first().addClass('active');
 });
