45e15bc1a2ce96338562058a824cf63fd079a5d3
fatihpur <lojmangrisi@gmail.com>
2023-08-06 18:42:29 +0300

    account open ok

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const searchinput = document.querySelector(".searchinput");
 const searchbox = document.querySelector(".opened-searchbox-container");
 const cancelicon = document.querySelector(".cancel-icon");
 const hugething = document.querySelector(".hugething");
+const accountopener = document.querySelector(".account-opener");
+const accountcontainer = document.querySelector("account-container");
 searchinput.addEventListener("click", () => {
   $(searchbox).toggleClass("showy");
   $(cancelicon).toggleClass("showy");
@@ -10,12 +12,10 @@ cancelicon.addEventListener("click", () => {
   $(searchbox).toggleClass("showy");
   $(cancelicon).toggleClass("showy");
 });
-/* document.onclick = (e) => {
-  const notincludes = !searchbox.classList.contains("show");
-  if (notincludes == true && e.target != searchbox) {
-    $(searchbox).addClass("show");
-  }
-}; */
 hugething.addEventListener("click", () => {
   $(searchbox).addClass("showy");
+  $(hugething).toggleClass("showy");
+});
+accountopener.addEventListener("click", () => {
+  $(accountcontainer).toggleClass("showy");
 });
