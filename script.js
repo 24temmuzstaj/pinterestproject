1fcc63600592e5a014ca5cdfe7b6ffc23b7d7b65
fatihpur <lojmangrisi@gmail.com>
2023-08-06 20:00:03 +0300

    update start

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const searchbox = document.querySelector(".opened-searchbox-container");
 const cancelicon = document.querySelector(".cancel-icon");
 const hugething = document.querySelector(".hugething");
 const accountopener = document.querySelector(".account-opener");
-const accountcontainer = document.querySelector("account-container");
+const accountcontainer = document.querySelector(".account-container");
 searchinput.addEventListener("click", () => {
   $(searchbox).toggleClass("showy");
   $(cancelicon).toggleClass("showy");
+  $(hugething).toggleClass("showy");
 });
 cancelicon.addEventListener("click", () => {
   $(searchbox).toggleClass("showy");
