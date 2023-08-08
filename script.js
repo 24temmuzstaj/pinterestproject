78585223f45badd103831b768a666798e0ad5c3f
fatihpur <lojmangrisi@gmail.com>
2023-08-08 21:47:55 +0300

    grid ok, açılır bölümler revize

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ const updatecontainer = document.querySelector(".update-container");
 const image = document.querySelector(".picture-container-image");
 const imageparent = document.querySelector(".picture-container");
 
-searchinput.addEventListener("click", () => {
+searchinput.addEventListener("click", (e) => {
+  e.stopPropagation();
   $(searchbox).toggleClass("showy");
   $(cancelicon).toggleClass("showy");
   $(hugething).toggleClass("showy");
@@ -22,17 +23,35 @@ hugething.addEventListener("click", () => {
   $(searchbox).addClass("showy");
   $(hugething).toggleClass("showy");
 });
-accountopener.addEventListener("click", () => {
+accountopener.addEventListener("click", (e) => {
+  e.stopPropagation();
   $(accountcontainer).toggleClass("showy");
 });
 
-updateopener.addEventListener("click", () => {
+updateopener.addEventListener("click", (e) => {
+  e.stopPropagation();
   $(updatecontainer).toggleClass("showy");
 });
 
-if (!updatecontainer.classList.contains("showy")) {
-  document.addEventListener("click", function (e) {
-    $(updatecontainer).toggleClass("showy");
+document.addEventListener("click", function (e) {
+  if (
+    searchbox.classList.contains("showy") ||
+    accountcontainer.classList.contains("showy") ||
+    updatecontainer.classList.contains("showy") ||
+    hugething.classList.contains("showy")
+  ) {
+    searchbox.classList.remove("showy");
+    accountcontainer.classList.remove("showy");
+    updatecontainer.classList.remove("showy");
+    hugething.classList.remove("showy");
     console.log("selam");
-  });
+  }
+});
+for (let i = 0; i < 144; i++) {
+  const element = document.createElement("div");
+  element.classList.add("masonry-item");
+  const image = document.createElement("img");
+  image.src = "./img/" + i + ".jpg";
+  element.append(image);
+  imageparent.append(element);
 }
