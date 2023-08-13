77828903ff046f2bee7f73ab25cb661a94a6869d
mmehtapavci <mmmehtapavci@gmail.com>
2023-08-13 22:47:56 +0300

    login form final
    Oturum aç tıklandığındaki formun bitmiş hali

--- static/script.js
+++ static/script.js
@@ -14,37 +14,7 @@ $(document).ready(function() {
     var interval = setInterval(nextSlide, 5000);
 });
 
-
-/* OTURUM AÇ BUTONUNA TIKLANINCA AÇILAN BÖLÜMÜN JS KODULARI */
-const loginbutton = document.querySelector(".nav-oturum-ac");
-const registerbutton = document.querySelector(".nav-kaydol");
-const login = document.querySelector(".navbar-login-side");
-const register = document.querySelector(".navbar-register-side");
-const exitbutton = document.querySelector(".exit-button");
-const register_exit_button = document.querySelector(".register-exit-button");
-
-loginbutton.addEventListener("click", loginswitch);
-registerbutton.addEventListener("click", registerswitch);
-exitbutton.addEventListener("click", loginexit);
-register_exit_button.addEventListener("click", registerexit);
-
-function loginswitch() {
-  $(login).toggleClass("show");
-}
-function registerswitch() {
-  $(register).toggleClass("show");
-}
-function loginexit(event) {
-  $(login).toggleClass("show");
-  event.preventDefault();
-}
-function registerexit(event) {
-  $(register).toggleClass("show");
-  event.preventDefault();
-}
-
-
-
+// PAROLA GİZLE GÖSTER BUTONU 
 document.addEventListener("DOMContentLoaded", function() {
   const passwordInput = document.getElementById("password");
   const showButton = document.getElementById("showButton");
