ad3817ab0629a9394b40eb364b8ee2def7cdc305
mmehtapavci <mmmehtapavci@gmail.com>
2023-08-13 21:59:44 +0300

    login form edits

--- static/script.js
+++ static/script.js
@@ -43,3 +43,22 @@ function registerexit(event) {
   event.preventDefault();
 }
 
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const passwordInput = document.getElementById("password");
+  const showButton = document.getElementById("showButton");
+  const eyeIcon = showButton.querySelector("i");
+
+  showButton.addEventListener("click", function() {
+      if (passwordInput.type === "password") {
+          passwordInput.type = "text";
+          eyeIcon.classList.remove("fa-eye-slash");
+          eyeIcon.classList.add("fa-eye");
+      } else {
+          passwordInput.type = "password";
+          eyeIcon.classList.remove("fa-eye");
+          eyeIcon.classList.add("fa-eye-slash");
+      }
+  });
+});
