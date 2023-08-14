7e0caca65a90878ef0d9c5645b70abf1d6d33ed2
seriolgun <z.olgun14@gmail.com>
2023-08-14 23:43:56 +0300

    mehtap form güncellemeleri çekildi

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Otomatik geçişi başlat
     $('#carouselExampleCaptions').carousel();
 
@@ -15,3 +15,21 @@ $(document).ready(function() {
 });
 
 
+// PAROLA GİZLE GÖSTER BUTONU 
+document.addEventListener("DOMContentLoaded", function () {
+    const passwordInput = document.getElementById("password");
+    const showButton = document.getElementById("showButton");
+    const eyeIcon = showButton.querySelector("i");
+
+    showButton.addEventListener("click", function () {
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            eyeIcon.classList.remove("fa-eye-slash");
+            eyeIcon.classList.add("fa-eye");
+        } else {
+            passwordInput.type = "password";
+            eyeIcon.classList.remove("fa-eye");
+            eyeIcon.classList.add("fa-eye-slash");
+        }
+    });
+});
