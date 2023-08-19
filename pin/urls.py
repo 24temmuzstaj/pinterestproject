4a4b27bde2b722aa0ed16e428d367993fb3096e3
seriolgun <z.olgun14@gmail.com>
2023-08-20 00:48:10 +0300

    kullanıcılara pin oluşturma eklendi

--- pin/urls.py
+++ pin/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
+from pin.views import *
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns =[
     path('', views.index, name='index'),
-
+    path('pin-detay/<int:pinId>/', views.pins, name='detay'),
+    path('create/',views.create, name='create'),
 
 ]
 
--- pin/views.py
+++ pin/views.py
@@ -1,8 +1,32 @@
 
 
-from django.shortcuts import render
-from .models import Pins
+from django.shortcuts import render, redirect
+from .models import *
 
 def index(request):
     pins = Pins.objects.all()  # Değişken adını 'pins' olarak düzeltiyoruz
     return render(request, 'index.html', {'pins': pins})
+
+def pins(request,pinId):
+    pinim=Pins.objects.get(id=pinId)
+    context={
+        'pin': pinim
+    }
+    return render(request, 'detail.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        title = request.POST['title']  
+        description = request.POST['description']  
+        image = request.FILES['image']       
+        
+        pins = Pins.objects.create(
+            title=title, 
+            description=description,
+            image=image,
+        )
+        print("pins oluşturuldu")
+        pins.save()
+        return redirect('index')
+    return render(request, 'create.html')
