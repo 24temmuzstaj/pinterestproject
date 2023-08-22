30e9fe28f5302fb77313a0aa5727815289874c55
fatihpur <lojmangrisi@gmail.com>
2023-08-22 19:16:32 +0300

    fotoğrafların üzerindeki kaydet ve detay butonları revize edildi

---
+++ static/pin.js
@@ -0,0 +1,26 @@
+const pin = document.querySelectorAll(".imageandtext");
+const pinfunc = () => {
+  pin.forEach((element) => {
+    const txt = document.createElement("button");
+    txt.className = "pin-button";
+    txt.innerHTML = "Kaydet";
+    const pintxt = document.createElement("button");
+    pintxt.className = "pin-left-button";
+    pintxt.innerHTML = "Detay";
+    pintxt.addEventListener("click", (e) => {
+      e.stopPropagation();
+    });
+
+    element.append(txt, pintxt);
+    console.log("selam");
+  });
+};
+pinfunc();
+
+const pinsave = document.querySelectorAll(".pin-left-span");
+const profilesaver = document.querySelector(".profile-save-container");
+pinsave.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    $(pinsave).toggleClass("showwy");
+  });
+});
