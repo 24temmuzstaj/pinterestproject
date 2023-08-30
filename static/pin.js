f4f23219cea6a80bb4b0b331972108e7d7546935
fatihpur <lojmangrisi@gmail.com>
2023-08-30 15:02:14 +0300

    header a devam edilecek

--- static/pin.js
+++ static/pin.js
@@ -5,18 +5,27 @@ const pinfunc = () => {
     txt.className = "pin-button";
     txt.innerHTML = "Kaydet";
     $(txt).attr("href", "{% URL 'profile' %}");
+    txt.addEventListener("click", (event) => {
+      event.preventDefault();
+    });
     const pintxt = document.createElement("button");
     pintxt.className = "pin-left-button";
     pintxt.innerHTML = "Detay";
-    pintxt.addEventListener("click", (e) => {
-      e.stopPropagation();
-    });
-
+    pintxt.addEventListener("click", stopPr, false);
     element.append(txt, pintxt);
     console.log("selam");
   });
+  function stopPr(event) {
+    event.preventDefault();
+  }
 };
 pinfunc();
+/* const txt = document.querySelector(".pin-button");
+$(document).ready(function () {
+  txt.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+}); */
 
 const pinsave = document.querySelectorAll(".pin-left-span");
 const profilesaver = document.querySelector(".profile-save-container");
--- static/script.js
+++ static/script.js
@@ -1,17 +1,15 @@
 $(document).ready(function () {
-    // Otomatik geçişi başlat
-    $('#carouselExampleCaptions').carousel();
+  // Otomatik geçişi başlat
+  $("#carouselExampleCaptions").carousel();
 
-    // Otomatik geçiş için fonksiyon
-    function nextSlide() {
-        var activeIndex = $('.carousel-item.active').index();
-        var slideCount = $('.carousel-item').length;
-        var nextIndex = (activeIndex + 1) % slideCount;
-        $('#carouselExampleCaptions').carousel(nextIndex);
-    }
+  // Otomatik geçiş için fonksiyon
+  function nextSlide() {
+    var activeIndex = $(".carousel-item.active").index();
+    var slideCount = $(".carousel-item").length;
+    var nextIndex = (activeIndex + 1) % slideCount;
+    $("#carouselExampleCaptions").carousel(nextIndex);
+  }
 
-    // Otomatik geçiş aralığı (3 saniye)
-    var interval = setInterval(nextSlide, 5000);
+  // Otomatik geçiş aralığı (3 saniye)
+  var interval = setInterval(nextSlide, 5000);
 });
-
-
