6345203c681a44715b84883c0ea22d6e4d959aa4
mmehtapavci <mmmehtapavci@gmail.com>
2023-09-02 14:44:02 +0300

    edit_profile sayfası yapımı

--- static/script.js
+++ static/script.js
@@ -14,4 +14,26 @@ $(document).ready(function () {
     var interval = setInterval(nextSlide, 5000);
 });
 
+// Mehtap edit_profile start
+function goster(icerikAdi) {
+    $('#rightboxone h4').text(icerikAdi);
+    
+    // İçerik adına göre içeriği güncelleyebilirsiniz. Örnek içerikler ekledim.
+    if (icerikAdi === 'Profili Düzenle') {
+        $('#rightboxone p').text('Kişisel bilgilerinizi gizli tutun. Buraya eklediğiniz bilgiler, profilinizi görüntüleyebilen herkes tarafından görülebilir.');
+        // <!-- 2.kutu -->
+    } else if (icerikAdi === 'Kişisel Bilgiler') {
+        $('#rightboxtwo p').text('fghjgfdgfh');
+    }
+    else if (icerikAdi === 'Hesap Bilgileri') {
+        $('#rightboxone p').text('Hesap Bilgileriniz burada görüntülenecektir.');
+    }
+    else if (icerikAdi === 'Ana Sayfa Akışı') {
+        $('#rightboxone p').text('anasayfaa.');
+    }
+    else if (icerikAdi === 'Hesaplar') {
+        $('#rightboxone p').text('anasayfaa.');
+    }
 
+    $('#rightboxone').show();
+}
