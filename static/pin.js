cece969bfed12bc03f9cf99b459c31314cd046c0
fatihpur <lojmangrisi@gmail.com>
2023-09-04 00:55:36 +0300

    butonlara javascript üzerinden href eklendi, kaydedilen pinlerden kaydet butonu kaldırıldı

--- static/pin.js
+++ static/pin.js
@@ -1,32 +1,21 @@
 const pin = document.querySelectorAll(".imageandtext");
 const pinfunc = () => {
   pin.forEach((element) => {
-    const txt = document.createElement("button");
+    const txt = document.createElement("a");
     txt.className = "pin-button";
     txt.innerHTML = "Kaydet";
-    $(txt).attr("href", "{% URL 'profile' %}");
-    txt.addEventListener("click", (event) => {
-      event.preventDefault();
-    });
+    $(txt).attr("href", "{% url 'save_pin' pin.id %}");
+    // $(txt).attr("href", "#");
+
     const pintxt = document.createElement("button");
     pintxt.className = "pin-left-button";
     pintxt.innerHTML = "Detay";
-    pintxt.addEventListener("click", stopPr, false);
+
     element.append(txt, pintxt);
     console.log("selam");
   });
-  function stopPr(event) {
-    event.preventDefault();
-  }
 };
 pinfunc();
-/* const txt = document.querySelector(".pin-button");
-$(document).ready(function () {
-  txt.addEventListener("click", (e) => {
-    e.stopPropagation();
-  });
-}); */
-
 const pinsave = document.querySelectorAll(".pin-left-span");
 const profilesaver = document.querySelector(".profile-save-container");
 pinsave.forEach((element) => {
